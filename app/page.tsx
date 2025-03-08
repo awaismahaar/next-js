34ef9382b5fe91e758a62e4d31af75856991de0a
Muhammad Awais <awaismahaar499@gmail.com>
2025-03-08 19:41:32 +0500

    add postinstall

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,10 @@ async function getData() {
   })
   return data;
 }
+type Post = {
+  id: string;
+  title: string;
+};
 
 export default async function Home() {
   const data = await getData()
@@ -17,9 +21,9 @@ export default async function Home() {
     <div className="py-6">
       <h1 className="text-2xl md:text-5xl tracking-tight font-bold">Latest Posts</h1>
      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {data?.map((item)=>(
-        <div key={item.id}>
-          <p>{item.title}</p>
+      {data?.map((item:Post)=>(
+        <div key={item?.id}>
+          <p>{item?.title}</p>
         </div>
       ))}
      </div>
